c8ca82032a1704db4d57a3744d0f5b547ce27e0f
Server <server@server.com>
2015-11-10 06:31:41 +0000

    feature1 test message

---
+++ src/com/scrape/exceptions/ScrapeExceptionTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.scrape.exceptions;
+/**
+ *
+ * @author  Cristi Bucnari
+ * test exception 
+ */
+public class ScrapeExceptionTest extends Exception {
+ //
+ public ScrapeException(String message){
+  super(message);
+ }
+
+}
